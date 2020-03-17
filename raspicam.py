5d640d55818479edfb92509f139059d3e4e850fb
davidking4411 <52112674+davidking4411@users.noreply.github.com>
2020-03-17 13:32:51 -0400

    Update raspicam.py

--- raspicam.py
+++ raspicam.py
@@ -9,12 +9,8 @@ import requests
 
 # initialize the camera and grab a reference to the raw camera capture
 ad_history = [0,0,0,0]
-#lower_green = np.array([20, 50,48]) # HSV values
-#upper_green = np.array([50, 260, 255])
 green = np.uint8([[[49, 166, 187]]])
 hsvGreen = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-lower_green = np.uint8([hsvGreen[0][0][0]-10,50,48])
-upper_green = np.uint8([hsvGreen[0][0][0]+10,255,255])
 lower_green = np.uint8([hsvGreen[0][0][0]-10,50,50])
 upper_green = np.uint8([hsvGreen[0][0][0]+10,255,255])
 
@@ -34,20 +30,6 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(.25)
 camera.exposure_mode = 'off'
 camera.meter_mode = 'spot'
-
-# # allow the camera to warmup
-# time.sleep(0.1)
-# # grab an image from the camera
-# camera.capture(rawCapture, format="bgr")
-# image = rawCapture.array
-# #image = cv2.imread('foo.jpg')
-# hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# mask = cv2.inRange(hsv, lower_green, upper_green)
-# print(sum(sum(mask)))
-# # cv2.imshow('a',image)
-# # cv2.waitKey(0)
-# # cv2.imshow('a',mask)
-# # cv2.waitKey(0)
 
 try:
     requests.post('http://192.168.1.20:8060/keypress/VolumeUp')
