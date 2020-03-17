2824d751f041180a1175a3cd54a9d9cbc0573a2a
davidking4411 <52112674+davidking4411@users.noreply.github.com>
2020-03-17 14:23:01 -0400

    Add files via upload

--- raspicam.py
+++ raspicam.py
@@ -7,16 +7,28 @@ import numpy as np
 import xmltodict
 import requests
 
+def get_active_app():
+    response = requests.get('http://192.168.1.20:8060/query/active-app')
+    response_dict = xmltodict.parse(response.text)
+    try: #if on home screen 'app' is last field so it'll error out
+        return response_dict['active-app']['app']['#text']
+    except:
+        return 'Home'
+
 # initialize the camera and grab a reference to the raw camera capture
 ad_history = [0,0,0,0]
-green = np.uint8([[[49, 166, 187]]])
+#lower_green = np.array([20, 50,48]) # HSV values
+#upper_green = np.array([50, 260, 255])
+green = np.uint8([[[49, 166, 187]]]) # GRB
 hsvGreen = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+lower_green = np.uint8([hsvGreen[0][0][0]-10,50,48])
+upper_green = np.uint8([hsvGreen[0][0][0]+10,255,255])
 lower_green = np.uint8([hsvGreen[0][0][0]-10,50,50])
 upper_green = np.uint8([hsvGreen[0][0][0]+10,255,255])
 
 #for rectangle
-start_point = (int(640/2-75), int(480/2-75)) 
-end_point = (int(640/2+75), int(480/2+75)) 
+start_point = (int(640/2-75), int(480/2-70)) 
+end_point = (int(640/2+75), int(480/2+70)) 
 color = (0, 255, 0) 
 thickness = 2
 
@@ -30,25 +42,22 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(.25)
 camera.exposure_mode = 'off'
 camera.meter_mode = 'spot'
-
+active_app = get_active_app()
 try:
     requests.post('http://192.168.1.20:8060/keypress/VolumeUp')
     requests.post('http://192.168.1.20:8060/keypress/VolumeDown')
-    response = requests.get('http://192.168.1.20:8060/query/active-app')
-    response_dict = xmltodict.parse(response.text)
-    response_dict['active-app']['app']['#text']
+    
+    
 except KeyboardInterrupt:
     print('unmuting failed')
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
-    #rawCapture = PiRGBArray(camera)
-    #time.sleep(.5)
-    #camera.capture(rawCapture, format='bgr')
+
     image = rawCapture.array
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_green, upper_green)
-    cropped_mask = mask[245:395,165:315]
+    cropped_mask = mask[245:395,170:310]
     image_w_rec = cv2.rectangle(image, start_point, end_point, color, thickness)
     cv2.imshow('mask',mask)
     cv2.imshow('image',image_w_rec)
@@ -56,9 +65,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     ad_history.pop(0)
     ad_history.append(sum_mask)
     print('history mean', np.mean(ad_history))
-    if (10000 <= np.mean(ad_history)) and (np.mean(ad_history) <= 13000):
+    if 10000 <= np.mean(ad_history) and np.mean(ad_history) <=13500:
            print('ad: '+ str(sum_mask))
-           if not muted and response_dict['active-app']['app']['#text'] == 'Hulu':
+           if not muted and active_app == 'Hulu':
                print('muting')
                requests.post('http://192.168.1.20:8060/keypress/VolumeUp')
                requests.post('http://192.168.1.20:8060/keypress/VolumeDown')
@@ -74,10 +83,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
            print('not ad: '+ str(sum_mask))
 
 
-    if time.time()-check_time > 60*5:
-        response = requests.get('http://192.168.1.20:8060/query/active-app')
-        response_dict = xmltodict.parse(response.text)
-    
+    if time.time()-check_time > 60*2: # check if Hulu is even the running app
+        active_app = get_active_app()
+        while active_app != 'Hulu':
+            #don't bother getting another frame from the camera if Hulu isn't the current app
+            print('waiting for Hulu app to start')
+            time.sleep(60*3)
+            active_app = get_active_app()
+
     key = cv2.waitKey(1)
     rawCapture.truncate(0)
     if key == 27: # esacpe key
